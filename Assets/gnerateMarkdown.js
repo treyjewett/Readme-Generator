23ae61ddba2390d6e2a838b24c9f324913d4d32a
Russell Jewett <rfjewett3@gmail.com>
2021-01-04 21:53:57 -0500

    adds prompts and populates markdown page

--- Assets/gnerateMarkdown.js
+++ Assets/gnerateMarkdown.js
@@ -1,7 +1,50 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
 
+  ${data.license[0]}
+  
+  # ${data.title}
+
+  ## Description:
+
+  ${data.description}
+
+  ## Table of Contents
+  ### [Installation Instructions:](#installation-instructions:)
+  ### [Usage Information:](#usage-information:)
+  ### [Contribution Guidelines:](#contribution-guidelines:)
+  ### [Testing Instructions:](#testing-instructions:)
+  ### [License:](#license:)
+  ### [Questions:](#questions:)
+
+  ## Installation Instructions:
+
+  ${data.instructions}
+
+  ## Usage Information:
+
+  ${data.usage}
+
+  ## Contribution Guidelines:
+
+  ${data.contribution}
+
+  ## Testing Instructions:
+
+  ${data.test}
+
+  ## License: ${data.license[0]}
+
+  This project is using ${data.license[1]}. For more information, click the badge.
+
+  ## Questions:
+
+  ### Please contact me if you have any questions:
+
+  ${data.github}
+
+  ${data.email}
 `;
 }
 
--- Assets/index.js
+++ Assets/index.js
@@ -1,15 +1,85 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-];
+const markdown = require("./gnerateMarkdown");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Briefly describe your project: '
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Give instuctions for installing the program here: '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What information is needed in order to utilize the project?'
+        },
+        {
+            type: 'input',
+            name: 'contubution',
+            message: 'List contribution guidelines on your project here: '
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter instructions for testing here: '
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license to use for you project.',
+            choices: ['Apache 2.0', 'MIT', 'ISC', 'BSD 2.0', 'Eclipse 2.0', 'GNU Affero GPL v3.0', 'GNU GPL v2.0']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your github username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            messge: 'What is your email address?'
+        }
+]);
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
+async function init() {
+    try {
+        const data = await questions;
 
+        contactInfo(data);
+        licenseChoice(data);
+
+        const readMe = generateMarkdown(data);
+
+        await writeFileAsync('readMe.md', readMe);
+
+        console.log('Successfully wrote to readMe.md');
+
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 // function call to initialize program
